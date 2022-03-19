255fe71c54ae17bb2c08829b58adebbe6ac59d5d
ShubhrankR <shubhrankrastogi@gmail.com>
2022-03-20 03:34:16 +0530

    Initial stage of app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {WrapperComponent} from "./layout/core/wrapper/wrapper.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: WrapperComponent,
+        loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule)
+    },
+    {
+        path: '**',
+        redirectTo: '404-not-found',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'multiplex-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'multiplex-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'multiplex';
+    title = 'multiplex';
 }
